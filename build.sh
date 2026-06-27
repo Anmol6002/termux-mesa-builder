ededba3242e3ea61970833fc83dc471eb0bbdb25
Anmol Singh <162159622+Anmol6002@users.noreply.github.com>
2026-06-27 16:51:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ TERMUX_PKG_DEPENDS="libc++, libxcb, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="glslang, spirv-tools, vulkan-headers, vulkan-loader-generic"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DRESOURCE_INSTALL_DIR=${TERMUX_PREFIX}/share/sascha-vulkan
+-DRESOURCE_INSTALL_DIR=${TERMUX_PREFIX}/share/sascha-vulkan-demos
 -DFORCE_VALIDATION=OFF
 "
 
@@ -33,23 +33,23 @@ termux_step_pre_configure() {
 
 termux_step_make_install() {
 	mkdir -p \
-		"${TERMUX_PREFIX}/libexec/sascha-vulkan" \
-		"${TERMUX_PREFIX}/share/sascha-vulkan"
+		"${TERMUX_PREFIX}/libexec/sascha-vulkan-demos" \
+		"${TERMUX_PREFIX}/share/sascha-vulkan-demos"
 
 	cp -a "${TERMUX_PKG_BUILDDIR}/bin/." \
-		"${TERMUX_PREFIX}/libexec/sascha-vulkan/"
+		"${TERMUX_PREFIX}/libexec/sascha-vulkan-demos/"
 
 	cp -a "${TERMUX_PKG_SRCDIR}/assets/." \
-		"${TERMUX_PREFIX}/share/sascha-vulkan/"
+		"${TERMUX_PREFIX}/share/sascha-vulkan-demos/"
 
 	cp -a "${TERMUX_PKG_SRCDIR}/shaders" \
-		"${TERMUX_PREFIX}/share/sascha-vulkan/"
+		"${TERMUX_PREFIX}/share/sascha-vulkan-demos/"
 }
 
 termux_step_post_make_install() {
 	local f name
 
-	for f in "${TERMUX_PREFIX}"/libexec/sascha-vulkan/*; do
+	for f in "${TERMUX_PREFIX}"/libexec/sascha-vulkan-demos/*; do
 		[ -f "${f}" ] || continue
 		[ -x "${f}" ] || continue
 
@@ -57,68 +57,7 @@ termux_step_post_make_install() {
 
 		cat > "${TERMUX_PREFIX}/bin/sascha-${name}" <<-WRAPPER
 		#!${TERMUX_PREFIX}/bin/sh
-		exec "${TERMUX_PREFIX}/libexec/sascha-vulkan/${name}" "\$@"
-		WRAPPER
-
-		chmod 755 "${TERMUX_PREFIX}/bin/sascha-${name}"
-	done
-}
-TERMUX_PKG_HOMEPAGE=https://github.com/SaschaWillems/Vulkan
-TERMUX_PKG_DESCRIPTION="C++ Vulkan examples and demos"
-TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_LICENSE_FILE="LICENSE.md"
-TERMUX_PKG_MAINTAINER="@termux"
-
-_COMMIT=b6ee8dc2dd275dcad4a1539b8b82eedef8487a85
-_COMMIT_DATE=20260502
-TERMUX_PKG_VERSION=0.0.${_COMMIT_DATE}
-
-TERMUX_PKG_SRCURL=git+https://github.com/SaschaWillems/Vulkan.git
-TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_SHA256=SKIP_CHECKSUM
-
-TERMUX_PKG_DEPENDS="libc++, libxcb, vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="glslang, spirv-tools, vulkan-headers, vulkan-loader-generic"
-
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DRESOURCE_INSTALL_DIR=${TERMUX_PREFIX}/share/sascha-vulkan
--DFORCE_VALIDATION=OFF
-"
-
-termux_step_post_get_source() {
-	# Fetch the full git history so we can check out historical commits
-	git fetch --unshallow || true
-	git checkout "${_COMMIT}"
-	git submodule update --init --recursive
-}
-
-termux_step_make_install() {
-	mkdir -p \
-		"${TERMUX_PREFIX}/libexec/sascha-vulkan" \
-		"${TERMUX_PREFIX}/share/sascha-vulkan"
-
-	cp -a "${TERMUX_PKG_BUILDDIR}/bin/." \
-		"${TERMUX_PREFIX}/libexec/sascha-vulkan/"
-
-	cp -a "${TERMUX_PKG_SRCDIR}/assets/." \
-		"${TERMUX_PREFIX}/share/sascha-vulkan/"
-
-	cp -a "${TERMUX_PKG_SRCDIR}/shaders" \
-		"${TERMUX_PREFIX}/share/sascha-vulkan/"
-}
-
-termux_step_post_make_install() {
-	local f name
-
-	for f in "${TERMUX_PREFIX}"/libexec/sascha-vulkan/*; do
-		[ -f "${f}" ] || continue
-		[ -x "${f}" ] || continue
-
-		name="$(basename "${f}")"
-
-		cat > "${TERMUX_PREFIX}/bin/sascha-${name}" <<-WRAPPER
-		#!${TERMUX_PREFIX}/bin/sh
-		exec "${TERMUX_PREFIX}/libexec/sascha-vulkan/${name}" "\$@"
+		exec "${TERMUX_PREFIX}/libexec/sascha-vulkan-demos/${name}" "\$@"
 		WRAPPER
 
 		chmod 755 "${TERMUX_PREFIX}/bin/sascha-${name}"
